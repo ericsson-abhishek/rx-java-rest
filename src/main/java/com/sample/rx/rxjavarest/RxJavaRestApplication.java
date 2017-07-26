f76b40e1c2c8d47cd40c5d761f3f7808dba80fd3
forum-abhi <mail2abhic@gmail.com>
2017-07-26 22:46:10 +0530

    Initial Commit

---
+++ src/main/java/com/sample/rx/rxjavarest/RxJavaRestApplication.java
@@ -0,0 +1,12 @@
+package com.sample.rx.rxjavarest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RxJavaRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RxJavaRestApplication.class, args);
+	}
+}
---
+++ src/test/java/com/sample/rx/rxjavarest/RxJavaRestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sample.rx.rxjavarest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RxJavaRestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
